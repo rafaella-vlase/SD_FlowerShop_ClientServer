8f467af98268b0266b46f4c763e236f7d1c033cb
rafaella-vlase <kmyelly2@gmail.com>
2024-06-05 15:54:32 +0300

    not done statisticsservice

--- SD_FlowerShop_Server/SD_FlowerShop_Server/Service/StatisticsService.cs
+++ SD_FlowerShop_Server/SD_FlowerShop_Server/Service/StatisticsService.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
+using SD_FlowerShop_Server.Domain;  
+using SD_FlowerShop_Server.Repository;
 
 namespace SD_FlowerShop_Server.Service
 {
-    internal class StatisticsService
+    public class StatisticsService
     {
+        private string criterion;
+        private List<Flower> flowerList;
+        private Dictionary<string, uint> statisticsResult;
+
+        public StatisticsService(string criterion)
+        {
+            this.criterion = criterion;
+            IFlowerRepository iFlowerRepository = new FlowerRepository();
+            this.flowerList = iFlowerRepository.FlowerList();
+            this.statisticsResult = new Dictionary<string, uint>();
+            if (this.flowerList != null && this.flowerList.Count > 0)
+            {
+                this.statisticalDetermination();
+            }
+        }
+
+        public string Criterion
+        {
+            get { return this.criterion; }
+            set { this.criterion = value; this.statisticalDetermination(); }
+        }
+
+        public List<Flower> FlowerList
+        {            
+            get { return this.flowerList; }
+            set { this.flowerList = value; this.statisticalDetermination(); }
+        }
+
+        public Dictionary<string, uint> StatisticsResult
+        {
+            get { return this.statisticsResult; }
+        }
+
+       
     }
 }
