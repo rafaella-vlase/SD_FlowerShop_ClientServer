db61117f860e9c58bc3ce4c879be4482d30b74af
rafaella-vlase <kmyelly2@gmail.com>
2024-06-05 12:24:34 +0300

    done domain

--- SD_FlowerShop_Server/SD_FlowerShop_Server/Domain/IFlowerRepository.cs
+++ SD_FlowerShop_Server/SD_FlowerShop_Server/Domain/IFlowerRepository.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SD_FlowerShop_Server.Domain
 {
-    internal interface IFlowerRepository
+    public interface IFlowerRepository
     {
+        bool AddFlower(Flower flower);
+        bool DeleteFlower(uint flowerID, uint shopID);
+        bool UpdateFlower(Flower flower);
+        DataTable FlowerTable();
+        DataTable FlowerTableEmpty();
+        DataTable FlowerTableEmployee(uint shopID);
+        List<Flower> FlowerListEmployee(uint shopID);
+        List<Flower> FlowerList();
+        List<Flower> FlowerList_ColorPrice(uint shopID);
+        List<Flower> FlowerList_Availability(uint shopID);
+        List<Flower> FlowerList_Price(string priceString, uint shopID);
+        List<Flower> FlowerList_Color(string color, uint shopID);
+        List<Flower> FlowerList_FlowerShop(uint shopID);
+        List<Flower> FlowerList_Stock(string stockString, uint shopID);
+        Flower SearchFlowerByName(string name);
+        List<Flower> SearchFlowerByType(string type);
     }
 }
--- SD_FlowerShop_Server/SD_FlowerShop_Server/Domain/IUserRepository.cs
+++ SD_FlowerShop_Server/SD_FlowerShop_Server/Domain/IUserRepository.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SD_FlowerShop_Server.Domain
 {
-    internal interface IUserRepository
+    public interface IUserRepository
     {
+        bool AddUser(User user);
+        bool LoginUser(string username, string password);
+        string GetRole(string username, string password);
+        string GetShopID(string username);
+        bool DeleteUser(uint userID);
+        bool UpdateUser(User user);
+        DataTable UserTable();
+        List<User> UserList();
+        List<User> UserList_Role(string searchedRole);
+        User SearchUserByID(string id);
+        List<User> SearchUserByUsername(string username);
+        List<User> SearchUserByRole(string role);
+
     }
 }
